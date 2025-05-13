bc9e003c24daea2161ac1317dbc32c71975cc5b8
mpilarski <tealdevmp@gmail.com>
2025-05-13 20:45:15 +0200

    chore: edit astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,20 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: node({
+    mode: "standalone", // można też użyć "middleware" w zależności od potrzeb
+  }),
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
   },
-  experimental: {},
+  experimental: {
+    session: true,
+  },
 });
